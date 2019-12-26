ca6821deea7c936d0d091d67b436fae184d72b38
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2019-12-26 11:54:07 +0100

    src/

---
+++ Practica3/src/script.bash
@@ -0,0 +1,26 @@
+totalTor=0
+totalVel=0
+for i in {1..100}
+do  
+    varSinMejora=$(./ejecutable $1 3)
+    varSinMejora="$( cut -d ':' -f 1 <<< "$varSinMejora" )"
+    varConMejoraTor=$(./ejecutable $1 3 1)
+    varConMejoraTor="$( cut -d ':' -f 1 <<< "$varConMejoraTor" )"
+    
+    if (( $(echo "$varSinMejora > $varConMejoraTor" |bc -l) ))
+    then
+        totalTor=$((totalTor+1))
+    fi
+    varConMejoraVel=$(./ejecutable $1 3 0 1)
+    varConMejoraVel="$( cut -d ':' -f 1 <<< "$varConMejoraVel" )"
+    if (( $(echo "$varSinMejora > $varConMejoraVel" |bc -l) ))
+    then
+        totalVel=$((totalVel+1))
+    fi
+    
+    echo $i
+
+done
+
+echo "Veces que es mejor con tormenta: $totalTor"
+echo "Veces que es mejor con velocidad: $totalVel"
